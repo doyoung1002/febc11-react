e91d9c2b70f1cc3d974cd56372555c96af35a3ec
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-11 15:13:18 +0900

    feat: Todolist - 컴포넌트 모듈화 기능 완성, vite로 프로젝트 생성

--- workspace/ch01-buildup/react/05/src/pages/TodoItem.jsx
+++ workspace/ch01-buildup/react/05/src/pages/TodoItem.jsx
@@ -1,9 +1,13 @@
-function TodoItem({ item }) {
+function TodoItem({ item, toggleDone, deleteItem }) {
   return (
     <li>
       <span>{item._id}</span>
-      <span>{item.done ? <s>{item.title}</s> : item.title}</span>
-      <button type='button'>삭제</button>
+      {/* 이벤트가 발생하면 그때 호출, 함수만 호출하게되면 return 값이 없기때문에 undefined로 나온다. 그래서 함수 객체를 전달해줘야 한다. */}
+      {/* toggleDone(item._id)로 전달하면 이는 return값이 없다. 이벤트 리스너는 함수를 등록하는 것이기 때문에, 함수 호출하는 코드를 작성하면 안된다. */}
+      <span onClick={() => toggleDone(item._id)}>{item.done ? <s>{item.title}</s> : item.title}</span>
+      <button type='button' onClick={() => deleteItem(item._id)}>
+        삭제
+      </button>
     </li>
   );
 }
