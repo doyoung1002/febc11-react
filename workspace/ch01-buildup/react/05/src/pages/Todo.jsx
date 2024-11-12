67b93f983753fa1862426a018fbe2f3ac1f46141
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-12 15:47:33 +0900

    feat: Counter - vite로 실습

--- workspace/ch01-buildup/react/05/src/pages/Todo.jsx
+++ workspace/ch01-buildup/react/05/src/pages/Todo.jsx
@@ -10,6 +10,8 @@ function Todo(props) {
             <h2>쇼핑 목록</h2>
             <TodoInput addItem={props.addItem} />
             <TodoList itemList={props.itemList} toggleDone={props.toggleDone} deleteItem={props.deleteItem} />
+            {/* 무조건 ...props로 넘기면 안된다. 최대한 필요한 것만 전달하는 것이 좋다. */}
+            {/* <TodoList {...props} /> */}
           </li>
         </ul>
       </div>
