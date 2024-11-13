a3c446d26228d60ae1ee023db264aaef0f3c3b17
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-13 11:46:09 +0900

    docs: function, 구조분해할당, 기본 데이터 타입, 참조형 타입(객체)

--- workspace/ch01-buildup/js-review/01.js
+++ workspace/ch01-buildup/js-review/01.js
@@ -3,5 +3,40 @@ var arr = [10, 20, 30];
 // 각 요소의 제곱값으로 구성된 새로운 배열 생성
 var arr2 = [100, 400, 900];
 
+// for
+var arr2 = [];
+for (let i = 0; i < arr.length; i++) {
+  arr2.push(arr[i] * arr[i]);
+};
+
+// for of(ES6)
+var arr2 = [];
+for (let item of arr) {
+  arr2.push(item * item);
+};
+
+// Array.prototype.forEach(), ES5
+var arr2 = [];
+// index 안쓰면 생략 가능
+arr.forEach(function (item, index) {
+  arr2.push(item * item);
+});
+
+//  Array.prototype.map(), ES6
+// map() 메서드는 배열을 순회하며 각 요소에 대해 콜백함수를 실행하고, 그 결과를 자동으로 새로운 배열에 저장해서 반환한다.
+// 원본 배열은 변경하지 않는다.
+// 항상 새로운 배열을 반환함
+// push 대신 return의 형태로 요소를 반환해야 한다.
+var arr2 = arr.map(function (item) {
+  return (item * item);
+});
+
+// Arrow function, ES6
+var arr2 = [];
+arr.forEach((item, index) => arr2.push(item * item));
+
+// map
+var arr2 = arr.map(item => (item * item));
+
 
 console.log(arr2);  // [100, 400, 900]
--- workspace/ch01-buildup/js-review/02.js
+++ workspace/ch01-buildup/js-review/02.js
@@ -1,9 +1,31 @@
 var itemList = [
-  { no: 1, todo: '두부', done: true},
-  { no: 2, todo: '계란', done: false},
-  { no: 3, todo: '라면', done: true},
+  { no: 1, todo: '두부', done: true },
+  { no: 2, todo: '계란', done: false },
+  { no: 3, todo: '라면', done: true },
 ];
 
 console.log(itemList[0]);
 console.log(itemList[1]);
 
+var [first, second] = itemList;
+console.log(first);
+console.log(second);
+
+// var state = useState(0);
+// console.log(state[0]);
+// console.log(state[1](state[0] + 1));
+
+// var [count, setCount] = useState(0);
+// console.log(count);
+// console.log(setCount(count + 1));
+
+console.log(second.no, second.todo);
+var { no, todo, done } = second;
+console.log(no, todo, done);
+
+// function App(props){};
+
+// function App({no, todo}){};
+
+// 구조분해할당에서 객체를 꺼내쓸 때는 순서는 상관이 없다.
+// 하지만 배열을 할 때는 index가 있기때문에 순서가 중요하다.
