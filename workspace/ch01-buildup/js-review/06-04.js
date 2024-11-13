7de7a27203c61adb8f5f736654404f8871e28d16
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-13 15:49:03 +0900

    docs: Promise(resolve, reject)

--- workspace/ch01-buildup/js-review/06-04.js
+++ workspace/ch01-buildup/js-review/06-04.js
@@ -1,33 +1,39 @@
 // 06-03.js 복사
 
-function f1(callback) {
-  console.log(`2. f1 작업 시작.`);
-  console.log(`3. f1 작업중...`);
+function f1() {
+  return new Promise((resolve, reject) => {
+    console.log(`2. f1 작업 시작.`);
+    console.log(`3. f1 작업중...`);
 
-  setTimeout(() => {
-    // ......
-    console.log(`4. f1 작업 종료.`);
-    callback(`f1의 결과물`);
-  }, 1000);
+    setTimeout(() => {
+      // ......
+      console.log(`4. f1 작업 종료.`);
+      // resolve 첫번째 인자는 작업이 성공적으로 끝날 때 호출할 함수 - 이름은 자유롭게 작성 가능
+      resolve(`f1의 결과물`);
+      // reject 두번째 인자는 에러가 발생했을 때 호출할 함수 - 이름은 자유롭게 작성 가능
+      reject(new Error('에러 발생'));
+    }, 1000);
+  });
 }
 
-function f2(f1Result, callback) {
-  console.log(`5. ${f1Result}로 f2 작업 시작.`);
-  console.log(`6. f2 작업중...`);
+function f2(f1Result) {
+  return new Promise((resolve, reject) => {
+    console.log(`5. ${f1Result}로 f2 작업 시작.`);
+    console.log(`6. f2 작업중...`);
 
-  setTimeout(() => {
-    // ......
-    console.log(`7. f2 작업 종료.`);
-    callback(`최종 결과물`);
-  }, Math.random() * 2000);
+    setTimeout(() => {
+      // ......
+      console.log(`7. f2 작업 종료.`);
+      resolve(`최종 결과물`);
+    }, Math.random() * 2000);
+  })
 }
 
 function test() {
-  f1((f1Result) => {
-    f2(f1Result, (result) => {
-      console.log(`8. ${result}`);
-    });
-  });
+  f1()
+    .then(f2)
+    .then((result) => console.log('8', result))
+    .catch((error) => console.error(error));
 }
 
 console.log('1. 테스트 시작.');
--- workspace/ch01-buildup/js-review/06-05.js
+++ workspace/ch01-buildup/js-review/06-05.js
@@ -1,2 +1,41 @@
 // 06-04.js 복사
 
+function f1() {
+  return new Promise((resolve, reject) => {
+    console.log(`2. f1 작업 시작.`);
+    console.log(`3. f1 작업중...`);
+
+    setTimeout(() => {
+      // ......
+      console.log(`4. f1 작업 종료.`);
+      // resolve 첫번째 인자는 작업이 성공적으로 끝날 때 호출할 함수 - 이름은 자유롭게 작성 가능
+      resolve(`f1의 결과물`);
+      // reject 두번째 인자는 에러가 발생했을 때 호출할 함수 - 이름은 자유롭게 작성 가능
+      reject(new Error('에러 발생'));
+    }, 1000);
+  });
+}
+
+function f2(f1Result) {
+  return new Promise((resolve, reject) => {
+    console.log(`5. ${f1Result}로 f2 작업 시작.`);
+    console.log(`6. f2 작업중...`);
+
+    setTimeout(() => {
+      // ......
+      console.log(`7. f2 작업 종료.`);
+      resolve(`최종 결과물`);
+    }, Math.random() * 2000);
+  })
+}
+
+function test() {
+  f1()
+    .then(f2)
+    .then((result) => console.log('8', result))
+    .catch((error) => console.error(error));
+}
+
+console.log('1. 테스트 시작.');
+test();
+console.log('9. 테스트 완료.');
