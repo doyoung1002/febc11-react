65b062ca9b9dddede1635a27a0f49173052c1eae
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-14 14:17:44 +0900

    docs: state 변경 시 바로 변경이 되는 것이 아닌 한꺼번에 반영됨, 콜백함수로 리턴 값으로 저장해두었다가 다음 호출때 인자로 전달하면 한번에 변경할 수 있다

--- workspace/ch02-start/state/01/src/components/Message.jsx
+++ workspace/ch02-start/state/01/src/components/Message.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 
-let count = 0;
+// 모듈 스코프 변수 x
+// 모듈스코프를 사용하게 되면 다른 컴포넌트에도 공유가 되기 때문에 값 변경에 대한 문제가 생김
+// let count = 0;
 
 export default function Message() {
-  const [msg, setMsg] = useState('');
+  // 지역 변수 x
+  // let count = 0;
 
+  // state 사용
+  const [count, setCount] = useState(0);
+  const [msg, setMsg] = useState('');
   const handleChange = (event) => {
     const inputMsg = event.target.value;
     setMsg(inputMsg);
-    count++;
+    setCount(count + 1);
   };
 
   return (
