6e439ad7d1891186c5346d541450bec5e616e496
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-14 16:05:07 +0900

    feat: 회원정보 변경, 주소 변경 세팅

---
+++ workspace/ch02-start/state/04/src/components/EditAddress.jsx
@@ -0,0 +1,20 @@
+export default function EditAddress({ addressBook, handleAddressChange }) {
+  const list = addressBook.map((address) => {
+    return (
+      <>
+        <div key={address.id}>
+          <label htmlFor={address.id}>{address.name}</label>
+          <input
+            id={address.id}
+            type='text'
+            name={address.name}
+            value={address.value}
+            onChange={handleAddressChange}
+          />
+        </div>
+      </>
+    );
+  });
+
+  return list;
+}
