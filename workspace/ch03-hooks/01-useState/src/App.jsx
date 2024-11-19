aac6c54f6238af0e179873794e00901705bb8c2e
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-19 15:52:36 +0900

    feat: hook useState

---
+++ workspace/ch03-hooks/01-useState/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [text, setText] = useState('');
+
+  return (
+    <>
+      <h1>01 useState - 상태 관리</h1>
+      <div>
+        <input
+          type='text'
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+        <br />
+        <span>입력 메세지: {text}</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
