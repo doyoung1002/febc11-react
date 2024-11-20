c8006be201d2498b67acc8032128cab11d6d4090
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-20 10:19:19 +0900

    feat: 상태 변경, 제어 컴포넌트

--- workspace/ch03-hooks/02-useEffect/src/App.jsx
+++ workspace/ch03-hooks/02-useEffect/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div id='app'>
       <Header />
-      <Counter />
+      {/* 컴포넌트 태그 사이에 기본값으로 설정할 수 있음 children으로 */}
+      <Counter>5</Counter>
     </div>
   );
 }
