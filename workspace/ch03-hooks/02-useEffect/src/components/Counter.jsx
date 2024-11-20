6c248e7b383ce4f3286bcf6e3f773d127b9cc803
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-20 11:30:05 +0900

    feat: useEffect, cleanup 함수 사용

--- workspace/ch03-hooks/02-useEffect/src/components/Counter.jsx
+++ workspace/ch03-hooks/02-useEffect/src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from './Button';
 import PropTypes from 'prop-types';
 
@@ -7,6 +7,7 @@ Counter.propTypes = {
 };
 
 function Counter({ children = '0' }) {
+  console.log('Counter 렌더링');
   const initCount = Number(children);
   const [count, setCount] = useState(initCount);
   const [step, setStep] = useState(1);
@@ -20,6 +21,63 @@ function Counter({ children = '0' }) {
   const handleReset = (event) => {
     setCount(initCount);
   };
+
+  // useEffect 사용 안했을 때
+  // 1초 후에 자동으로 값 한번 변경
+  // 무한 렌더링 발생
+  // setTimeout(() => {
+  //   handleUp();
+  // }, 1000);
+
+  // 마운트 된 후 최초 한번 증가 - useEffect
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log(
+  //     'dependencies에 빈 배열을 지정하면 컴포넌트가 마운트 된 후 한번만 호출됨(업데이트 후에는 호출되지 않음'
+  //   );
+  // }, []);
+
+  // 마운트 된 후와 업데이트 된 후 에값 증가 - useEffect
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependencies에 빈 배열을 지정하지 않으면 컴포넌트가 마운트 된 후, 업데이트 된 후에도 호출이 됨');
+  // });
+
+  // 마운트 된 후 값 증가 - useEffect
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log(
+  //     'dependencies에 값을 지정하면 컴포넌트가 업데이트 될 때 지정한 값 중에 하나라도 수정되었을 경우 호출됨 '
+  //   );
+  // }, [step]);
+
+  // useEffect(() => {
+  //   console.log('setup 함수 호출');
+  //   const timer = setInterval(() => {
+  //     console.log(new Date());
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
+
+  // setInterval, cleanup(clearInterval)
+  // useEffect(() => {
+  //   console.log(step, 'setup 함수 호출');
+  //   const timer = setInterval(() => {
+  //     console.log(step, new Date());
+  //   }, 1000);
+  //   return () => {
+  //     console.log(step, 'cleanup 함수 호출');
+  //     clearInterval(timer);
+  //   };
+  // }, [step]);
 
   return (
     <div id='counter'>
