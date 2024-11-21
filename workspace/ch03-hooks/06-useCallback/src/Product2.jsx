9b2d732aecd99dcda966d252d36720d34bc0c4a9
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-21 13:48:22 +0900

    feat: useMemo 추가

---
+++ workspace/ch03-hooks/06-useCallback/src/Product2.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { memo } from 'react';
+
+const Product2 = memo(function Product2({ product: { name, price, mainImage, content } }) {
+  console.log('Product 렌더링');
+  return (
+    <>
+      <h2>상품 설명</h2>
+      <p>상품명: {name}</p>
+      <p>가격: {price.toLocaleString()}</p>
+      <p>상품 설명</p>
+      <div>
+        <img
+          src={`https://11.fesp.shop/${mainImage}`}
+          width='600'
+        />
+        <p>{content}.</p>
+      </div>
+    </>
+  );
+});
+
+Product2.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    mainImage: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Product2;
