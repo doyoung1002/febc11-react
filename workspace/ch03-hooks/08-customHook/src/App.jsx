a165fd68d7cd4974708d6114514b3458548c821d
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-22 09:41:10 +0900

    feat: 에러메세지 추가

--- workspace/ch03-hooks/08-customHook/src/App.jsx
+++ workspace/ch03-hooks/08-customHook/src/App.jsx
@@ -2,7 +2,8 @@ const API_SERVER = 'https://todo-api.fesp.shop/api';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   // Todo API 서버에 ajax 요청을 보낸다.
   const fetchTodo = async (fetchParams) => {
@@ -13,24 +14,28 @@ function App() {
       console.log(jsonData);
       if (jsonData.ok) {
         setData(jsonData.items);
+        setError(null);
       } else {
         throw new Error(jsonData.error.message);
       }
     } catch (err) {
       // 에러 처리
       console.error(err);
+      setError({ message: '일시적인 문제로 인해 작업 처리에 실패했습니다. 잠시 후 다시 요청해 주시길 바랍니다.' });
+      setData(null);
     }
   };
 
   useEffect(() => {
-    const fetchParams = { url: '/todolist' };
+    const fetchParams = { url: '/todolist?delay=3000' };
     fetchTodo(fetchParams);
   }, []);
 
   return (
     <>
-      <h1>08 Custom Hook - fetch API 사용</h1>
+      <h1>08 Custom Hook - 커스텀 훅 없이 fetch API 사용</h1>
       <h2>할일 목록</h2>
+      {error && <p style={{ color: 'red' }}>{error.message}</p>}
       {data && (
         <ul>
           {data.map((item) => (
