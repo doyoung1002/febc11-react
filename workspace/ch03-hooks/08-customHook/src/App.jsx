d6882251ce5560eacb7a6fca81d6511836c53db4
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-22 10:18:42 +0900

    feat: custom hook useFetch 사용

--- workspace/ch03-hooks/08-customHook/src/App.jsx
+++ workspace/ch03-hooks/08-customHook/src/App.jsx
@@ -5,6 +5,7 @@ import { PacmanLoader } from 'react-spinners';
 function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  //error가 발생하면 화면도 갱신 되어야함 -> error를 화면에 출력해야함
   const [isLoading, setIsLoading] = useState(false);
 
   // Todo API 서버에 ajax 요청을 보낸다.
@@ -23,10 +24,11 @@ function App() {
       }
     } catch (err) {
       // 에러 처리
-      console.error(err);
+      console.error(err); // 서버로부터 데이터를 못받아올 때
       setError({ message: '일시적인 문제로 인해 작업 처리에 실패했습니다. 잠시 후 다시 요청해 주시길 바랍니다.' });
       setData(null);
     } finally {
+      // 무조건 호출되어야하는 코드는 finally에 넣어준다 - 마무리 느낌
       setIsLoading(false);
     }
   };
--- workspace/ch03-hooks/09-customHook/src/App.jsx
+++ workspace/ch03-hooks/09-customHook/src/App.jsx
@@ -1,44 +1,12 @@
-const API_SERVER = 'https://todo-api.fesp.shop/api';
-import React, { useEffect, useState } from 'react';
+import useFetch from '@hooks/useFetch';
 import { PacmanLoader } from 'react-spinners';
 
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  // Todo API 서버에 ajax 요청을 보낸다.
-  const fetchTodo = async (fetchParams) => {
-    try {
-      setIsLoading(true);
-      const res = await fetch(API_SERVER + fetchParams.url);
-      const jsonData = await res.json();
-      console.log(res);
-      console.log(jsonData);
-      if (jsonData.ok) {
-        setData(jsonData.items);
-        setError(null);
-      } else {
-        throw new Error(jsonData.error.message);
-      }
-    } catch (err) {
-      // 에러 처리
-      console.error(err);
-      setError({ message: '일시적인 문제로 인해 작업 처리에 실패했습니다. 잠시 후 다시 요청해 주시길 바랍니다.' });
-      setData(null);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    const fetchParams = { url: '/todolist?delay=3000' };
-    fetchTodo(fetchParams);
-  }, []);
+  const { data, error, isLoading } = useFetch({ url: '/todolist?delay=2000' });
 
   return (
     <>
-      <h1>09 Custom Hook - useFetch, useAxios 커스텀 훅 사용</h1>
+      <h1>09 Custom Hook - useFetch 커스텀 훅 사용</h1>
       <h2>할일 목록</h2>
       {isLoading && (
         <PacmanLoader
