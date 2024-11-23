d8bf36121eb21e50bb9c9f2566c024bd248fecba
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-23 17:45:36 +0900

    chore: 파일 세팅

---
+++ workspace/app/todolist/04-router/src/pages/TodoDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoDetail() {
+  return <div>TodoDetail</div>;
+}
+
+export default TodoDetail;
---
+++ workspace/app/todolist/04-router/src/pages/TodoEdit.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoEdit() {
+  return <div>TodoEdit</div>;
+}
+
+export default TodoEdit;
