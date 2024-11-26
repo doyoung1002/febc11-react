795f31320e472efee370b0127725bfdbe6f944aa
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-26 13:28:56 +0900

    feat: xhr로 에러처리 및 서버 응답

--- workspace/app/todolist/05-api/src/pages/TodoAdd.jsx
+++ workspace/app/todolist/05-api/src/pages/TodoAdd.jsx
@@ -5,21 +5,43 @@ function TodoAdd() {
   const {
     register,
     handleSubmit,
-    watch,
+    reset,
+    setFocus,
     formState: { errors },
     // mode: '', // 최초 검증 시점, default onSubmit
     // revalidateMode // 재검증 시점
     // criteriaMode // errors 객체에 첫 오류 하나만 포함하거나(firstError) 전부 포함(all), default firstError
-  } = useForm({
-    defaultValues: {
-      name: '',
-      email: '',
-      cellphone: '010',
-    },
-  });
+  } = useForm();
 
   const onSubmit = (item) => {
     console.log('서버에 전송', item);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://todo-api.fesp.shop/api/todolist');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.responseType = 'json'; // xhr.response에 지정되는 응답 데이터가 JSON.parse() 결과로 저장됨
+
+    // 서버로부터 응답이 도착하면 호출되는 함수
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        console.log(xhr.response);
+        alert('할 일이 추가되었습니다.');
+        setFocus('title');
+        reset();
+      } else {
+        // 4xx, 5xx
+        console.error('서버에서 에러 응답', xhr.status, xhr.response);
+        alert(xhr.response.error?.message || '할 일 추가에 실패했습니다.');
+      }
+    };
+
+    // 네트워크 오류는 따로 아래와 같이 onerror로 처리해야함
+    xhr.onerror = () => {
+      console.log('네트워크 오류');
+      alert('할 일 추가에 실패했습니다.');
+    };
+
+    xhr.send(JSON.stringify(item));
   };
 
   return (
@@ -32,10 +54,11 @@ function TodoAdd() {
             type='text'
             id='title'
             autoFocus
-            {...register('name', {
-              required: '이름을 입력하세요.',
+            {...register('title', {
+              required: '제목을 입력하세요.',
             })}
           />
+          <div className='input-error'>{errors.title?.message}</div>
           <br />
           <label htmlFor='content'>내용 :</label>
           <textarea
@@ -46,8 +69,9 @@ function TodoAdd() {
               required: '내용을 입력하세요.',
             })}
           />
+          <div className='input-error'>{errors.content?.message}</div>
           <br />
-          <Link to='/list/1'>추가</Link>
+          <button type='submit'>추가</button>
           <Link to='/list'>취소</Link>
         </form>
       </div>
