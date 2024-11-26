7fb8e03368e9af9ea66deae6898d5818880417ec
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-26 14:09:05 +0900

    feat: useAxiosInstance를 이용하여 훅을 바로 사용하지 못하니, 객체로 가져오는 법

--- workspace/app/todolist/05-api/src/pages/TodoEdit.jsx
+++ workspace/app/todolist/05-api/src/pages/TodoEdit.jsx
@@ -1,3 +1,4 @@
+import useAxiosInstance from '@hooks/useAxiosInstance';
 import { Link, useNavigate, useOutletContext } from 'react-router-dom';
 
 function TodoEdit() {
@@ -7,10 +8,14 @@ function TodoEdit() {
   // 프로그래밍 방식으로 페이지 이동에 사용
   const navigate = useNavigate();
 
+  // axios 인스턴스
+  const axios = useAxiosInstance();
+
   // 수정 작업
   const onSubmit = (event) => {
     try {
       event.preventDefault();
+      // hook은 이벤트에 의해서 호출되는 경우에는 사용이 불가능, 조회는 가능
       // TODO: API 서버에 수정 요청
 
       alert('할 일이 수정 되었습니다.');
