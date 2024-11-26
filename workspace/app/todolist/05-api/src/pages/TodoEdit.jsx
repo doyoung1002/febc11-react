61c581b11ea1c580257c805be632e2e546ee8451
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-26 14:23:23 +0900

    feat: useAxiosInstance로 객체로 가져와서 투두리스트 수정 기능 추가

--- workspace/app/todolist/05-api/src/pages/TodoEdit.jsx
+++ workspace/app/todolist/05-api/src/pages/TodoEdit.jsx
@@ -1,4 +1,5 @@
 import useAxiosInstance from '@hooks/useAxiosInstance';
+import { useForm } from 'react-hook-form';
 import { Link, useNavigate, useOutletContext } from 'react-router-dom';
 
 function TodoEdit() {
@@ -8,15 +9,29 @@ function TodoEdit() {
   // 프로그래밍 방식으로 페이지 이동에 사용
   const navigate = useNavigate();
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setFocus,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: item.title,
+      content: item.content,
+      done: item.done,
+    },
+  });
+
   // axios 인스턴스
   const axios = useAxiosInstance();
 
   // 수정 작업
-  const onSubmit = (event) => {
+  const onSubmit = async (formData) => {
     try {
-      event.preventDefault();
       // hook은 이벤트에 의해서 호출되는 경우에는 사용이 불가능, 조회는 가능
       // TODO: API 서버에 수정 요청
+      await axios.patch(`/todolist/${item._id}`, formData);
 
       alert('할 일이 수정 되었습니다.');
 
@@ -34,32 +49,38 @@ function TodoEdit() {
     <>
       <h2>할일 수정</h2>
       <div className='todo'>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor='title'>제목 :</label>
           <input
             type='text'
             id='title'
-            defaultValue={item.title}
             autoFocus
+            {...register('title', {
+              required: '제목을 입력하세요.',
+            })}
           />
+          <div className='input-error'>{errors.title?.message}</div>
           <br />
           <label htmlFor='content'>내용 :</label>
           <textarea
             id='content'
             cols='23'
             rows='5'
-            defaultValue={item.content}
+            {...register('content', {
+              required: '내용을 입력하세요.',
+            })}
           />
+          <div className='input-error'>{errors.content?.message}</div>
           <br />
           <label htmlFor='done'>완료 :</label>
           <input
             type='checkbox'
             id='done'
-            defaultChecked={item.done}
+            {...register('done')}
           />
           <br />
           <button type='submit'>수정</button>
-          <Link to='/list/1'>취소</Link>
+          <Link to={`/list/${item._id}`}>취소</Link>
         </form>
       </div>
     </>
