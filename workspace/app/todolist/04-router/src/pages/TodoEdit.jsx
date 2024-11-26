b7cf5684caebcc98d131877e68a1df4047581ab7
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-26 11:36:07 +0900

    feat: 수정, 삭제 기능 추가

--- workspace/app/todolist/04-router/src/pages/TodoEdit.jsx
+++ workspace/app/todolist/04-router/src/pages/TodoEdit.jsx
@@ -1,11 +1,29 @@
-import { Link, useOutletContext } from 'react-router-dom';
+import { Link, useNavigate, useOutletContext } from 'react-router-dom';
 
 function TodoEdit() {
   // Outlet 컴포넌트의 context 속성에 전달되는 값 추출
   const { item } = useOutletContext();
 
+  // 프로그래밍 방식으로 페이지 이동에 사용
+  const navigate = useNavigate();
+
   // 수정 작업
-  const onSubmit = () => {};
+  const onSubmit = (event) => {
+    try {
+      event.preventDefault();
+      // TODO: API 서버에 수정 요청
+
+      alert('할 일이 수정 되었습니다.');
+
+      // 할 일 상세보기로 이동
+      // navigate('..', { relative: true }); // 상대 경로로 이동
+      // navigate(`/list/${item._id}`, { replace: true }); // window.history.replaceState
+      navigate(-1); // navigate에 숫자를 전달하면 히스토리 back이 된다. window.history.back(-1)
+    } catch (err) {
+      console.error(err);
+      alert('할 일 수정에 실패했습니다.');
+    }
+  };
 
   return (
     <>
