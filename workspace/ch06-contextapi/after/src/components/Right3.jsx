a55e1821a164e7d92ea70b30313213396b85386b
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-27 15:54:27 +0900

    feat: simpleContext

--- workspace/ch06-contextapi/after/src/components/Right3.jsx
+++ workspace/ch06-contextapi/after/src/components/Right3.jsx
@@ -9,8 +9,10 @@ function Right3() {
 
   const simple = useContext(SimpleContext);
 
+  // CounterContext 구독함(CounterContext의 상태변경이 라렌더링을 유발함)
+  // CounterContext가 상태가 변하게 되면, 다른 Right1, 2는 구독하지 않았기에 렌더링이 되지 않는다
+  // 하지만 현재 Right3에서는 구독하기 때문에 자신도 같이 렌더링이 된다. 상태가 변하지 않아도 렌더링이 된다.
   const {
-    hello,
     actions: { countDown, reset, countUp },
   } = useContext(CounterContext);
 
