414b496e3e14918ecf567ee9266a3cc2589ff595
doyoung1002 <leedoyoung.oc@gmail.com>
2024-11-28 13:41:24 +0900

    feat: useSelector, useDispatch 를 이용하여 꺼내오고, 상태 변경하기 완료

--- workspace/ch07-globalstate/redux/src/components/Right3.jsx
+++ workspace/ch07-globalstate/redux/src/components/Right3.jsx
@@ -1,13 +1,24 @@
+import counterActionCreator from '@redux/counterActionCreator';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Right3() {
-  useEffect(()=>{
+  useEffect(() => {
     console.log('      # Right3 렌더링.');
   });
+
+  // 상태값 수정
+  //
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      {/* <button onClick={() => dispatch({ type: 'countUp', payload: { step: 2 } })}>+1</button> */}
+      {/* ActionCreator를 통해 아래 코드가 위와 같이 반환이 되도록 하는 것이다. */}
+      <button onClick={() => dispatch(counterActionCreator.countDown(1))}>-1</button>
+      <button onClick={() => dispatch(counterActionCreator.countReset())}>0</button>
+      <button onClick={() => dispatch(counterActionCreator.countUp(2))}>+2</button>
     </div>
   );
 }
