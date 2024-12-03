8c7b8cb829b34fab81cc57578a6b2831f54da445
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-03 11:14:03 +0900

    style: Tailwind의 base 레이어에 h1 재정의, components 레이어 재정의

--- workspace/ch08-css/04-tailwind/src/App.jsx
+++ workspace/ch08-css/04-tailwind/src/App.jsx
@@ -1,13 +1,12 @@
 import Button from '@components/Button';
-import './App.css';
 import Login from './Login';
 
 function App() {
   return (
     <>
-      <h1>01 CSS import 사용</h1>
+      <h1>04 TailWind CSS 사용</h1>
 
-      <div className='container'>
+      <div className='w-48 m-2 mx-auto'>
         <Button>그냥 버튼</Button>
         <Button
           bg='blue'
@@ -27,6 +26,9 @@ function App() {
         >
           회색 배경의 파란 글자
         </Button>
+        <button className='btn'>일반 버튼</button>
+        <button className='btn-primary'>등록</button>
+        <button className='btn-warn'>삭제</button>
       </div>
 
       <Login />
--- workspace/ch08-css/04-tailwind/src/components/Button.jsx
+++ workspace/ch08-css/04-tailwind/src/components/Button.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import './Button.css';
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
