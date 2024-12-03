2f42a2a7fd1a93796409d93510335ccd077ff506
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-03 14:46:29 +0900

    feat: loading, error 추가

--- workspace/ch09-ajax/01-nike/src/App.jsx
+++ workspace/ch09-ajax/01-nike/src/App.jsx
@@ -1,40 +1,44 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Product from './Product';
 import Shipping from './Shipping';
+import { DotLoader } from 'react-spinners';
 
 function App() {
-  console.log('App 렌더링'); // 1(맨처음)
-
-  // const data = useMemo(
-  //   () => ({
-  //     _id: 2,
-  //     price: 125000,
-  //     shippingFees: 3000,
-  //     name: '나이키 잼',
-  //     quantity: 35,
-  //     buyQuantity: 10,
-  //     mainImage: '/files/00-nike/NIKE_JAM_01.jpg',
-  //     content:
-  //       '나이키가 세계적인 무대에 오르는 브레이크 댄서를 위해 제작한 첫 신발인 잼과 함께 몸과 마음, 정신을 하나로 만들어 보세요. 신발의 모든 디테일을 꼼꼼히 제작했기 때문에 자신 있게 사이퍼에 도전할 수 있습니다. 유연하고 내구성이 뛰어난 갑피가 몸을 따라 움직이며, 중창의 텍스처 처리된 핸드 그립 덕분에 공중에서 신발을 쉽게 잡을 수 있습니다. 그리고 위아래가 뒤집힌 로고를 배치해 프리즈 동작을 할 때 로고가 똑바로 보이는 재미를 더했죠.',
-  //   }),
-  //   []
-
   const [data, setData] = useState(); // 1(마운트)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchData = async (_id) => {
-    const res = await fetch(`https://11.fesp.shop/products/${_id}`, {
-      headers: {
-        'client-id': '00-nike',
-      },
-    });
-    console.log('res', res);
-    const jsonData = await res.json();
-    console.log('jsonData', jsonData);
-    setData(jsonData.item); // 4번(마운트 후) - 화면 갱신
+    setIsLoading(true);
+    try {
+      const res = await fetch(`https://11.fesp.shop/123products/${_id}?delay=3000`, {
+        headers: {
+          'client-id': '00-nike',
+        },
+      });
+      console.log('res', res);
+      const jsonData = await res.json();
+      console.log('jsonData', jsonData);
+      if (res.ok) {
+        setData(jsonData.item); // 4번(마운트 후) - 화면 갱신
+        setError(null);
+      } else {
+        // 4xx, 5xx 응답 상태코드를 받을 경우
+        setError(jsonData);
+        setData(null);
+      }
+    } catch (err) {
+      // 네트워크 에러(fetch)
+      console.error(err);
+      setError({ message: '잠시 후 다시 요청하세요.' });
+      setData(null);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-    // 마운트 된 이후 호출 됨 3번
+    // 마운트 된 setUp함수가 호출 되고 fetchData가 실행됨
     fetchData(4); // 3번(마운트 된 후)
   }, []); // 마운트 된 이후에 최초 한번만 실행
 
@@ -59,6 +63,8 @@ function App() {
   return (
     <>
       <h1>01 NIKE 상품 상세 조회</h1>
+      {isLoading && <DotLoader />}
+      {error && <p>{error.message}</p>}
 
       {data && ( // 첫 마운트일 때 데이터가 없기 때문에 undefined
         <div>
