0c18ba12c60764916b36f4f2b118a92a39f1d545
doyoung1002 <leedoyoung.oc@gmail.com>
2024-12-16 13:03:23 +0900

    feat: 에러 메세지 출력

--- workspace/ch10-nextjs/next-board/src/app/[type]/ListItem.jsx
+++ workspace/ch10-nextjs/next-board/src/app/[type]/ListItem.jsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import React from 'react';
 
 export default function ListItem({ item }) {
+  // console.log(item);
   return (
     <tr className='border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out'>
-      <td className='p-2 text-center'>{item.user._id}</td>
+      <td className='p-2 text-center'>{item._id}</td>
       <td className='p-2 truncate indent-4'>
         <Link
           href={`/${item.type}/${item._id}`} // 절대경로
